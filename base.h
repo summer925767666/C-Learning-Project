fd47368c84aa14ca7a03ada36fc8d975934fd3c9
夏比天 <925767666@qq.com>
2019-03-06 15:24:59 +0800

    创建base.h头文件

---
+++ base.h
@@ -0,0 +1,44 @@
+#ifndef C_LEARNING_BASE_H
+#define C_LEARNING_BASE_H
+
+void Bool(void);
+
+void Comma(void);
+
+void RandGame(void);
+
+void SplitInt(void);
+
+void TestArray(void);
+
+void TestType(int x);
+
+void TestPointer(void);
+
+void Swap(int *x, int *y);
+
+void Array_2Dim(void);
+
+void IniArr(void);
+
+void TestStr(void);
+
+void GetPoint(void);
+
+void TestPoint(void);
+
+void ChangeValue(int *p);
+
+void PointerAndArray(void);
+
+void TestPointerAndArray(int *a, int **b);
+
+void CharAndStr(void);
+
+void Pointer_Array_String(void);
+
+void StrInput_OutPut(void);
+
+void StrFunc(void);
+
+#endif //C_LEARNING_BASE_H
